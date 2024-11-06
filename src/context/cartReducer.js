6a9e204c02014f33f7087810c90d268c4de4d5eb
kokonut <castaneda.jair.13@gmail.com>
2024-11-05 21:37:13 -0500

    add cartReducer.js

---
+++ src/context/cartReducer.js
@@ -0,0 +1,54 @@
+import { typesCart } from "./typesCart";
+
+
+export const cartReducer = (state, action) => {
+  //el type y payload que se envia con el dispatch
+  const { type: actionType, payload: actionPayload } = action;
+  switch (actionType) {
+    case typesCart.ADD_CART: {
+      /* El actionPayload seria el payload que envie, osea mi producto */
+      const { id } = actionPayload;
+      /* El state seria mi array cart */
+      const productInCart = state.findIndex((item) => item.id === id);
+
+      if (productInCart >= 0) {
+        return state.map((item) =>
+          item.id === id
+            ? { ...item, quantity: item.quantity + 1 } // Incrementa la cantidad
+            : item,
+        );
+      }
+
+      return [...state, { ...actionPayload, quantity: 1 }];
+    }
+    case typesCart.DELETE_TO_CART: {
+      const id = actionPayload;
+
+      return state.filter((item) => item.id !== id);
+    }
+    case typesCart.INCREMENT_CART: {
+      const id = actionPayload;
+
+      const cart = state.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item,
+      );
+
+      return cart;
+    }
+    case typesCart.DECREMENT_CART: {
+      const id = actionPayload;
+
+      const cart = state.map((item) =>
+        item.id === id && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item,
+      );
+
+      return cart;
+    }
+    case typesCart.CLEAR_CART: {
+    }
+    default:
+      return state;
+  }
+};
