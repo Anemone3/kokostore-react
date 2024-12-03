02e41b5bf4016373a4bf0746bf00abe70c64593f
kokonut <castaneda.jair.13@gmail.com>
2024-12-03 17:39:55 -0500

    git

--- old-page/ProductsPage.jsx
+++ old-page/ProductsPage.jsx
@@ -65,7 +65,7 @@
 //                   <div className="grid grid-cols-3 gap-4 h-full">
 //                 {products.map((product) => (
 //                   <ProductList key={product.id} {...product} />
-//                 ))}
+//                 ))}s
 //               </div>
 //             )}
 //           </section>
